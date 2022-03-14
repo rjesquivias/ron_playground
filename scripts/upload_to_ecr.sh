55ca068082067e0dee9e1e98011840f1c71295f3
RJ Esquivias <esquiviasrj@gmail.com>
2022-03-14 00:30:35 -0700

    Remove ecr upload script

--- scripts/upload_to_ecr.sh
+++
@@ -1,5 +0,0 @@
-#!/bin/bash
-docker build . -t rjcapuchin/ron_playground
-docker tag rjcapuchin/ron_playground:latest 510722588970.dkr.ecr.us-west-2.amazonaws.com/images:latest
-aws ecr get-login-password --region us-west-2 | docker login --username AWS --password-stdin 510722588970.dkr.ecr.us-west-2.amazonaws.com
-docker push 510722588970.dkr.ecr.us-west-2.amazonaws.com/images:latest
