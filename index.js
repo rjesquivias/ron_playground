af9a1b2fb8dac845d22f34726d4dca3ee92f6efb
RJ Esquivias <esquiviasrj@gmail.com>
2022-03-13 18:52:39 -0700

    Initial node & docker setup

---
+++ index.js
@@ -0,0 +1,30 @@
+var express = require('express');
+var { graphqlHTTP } = require('express-graphql');
+var { buildSchema } = require('graphql');
+
+const port = 80
+
+// Construct a schema, using GraphQL schema language
+var schema = buildSchema(`
+    type Query {
+        hello: String
+    }
+`);
+
+// The root providers a resolver function for each API endpoint
+var root = {
+    hello: () => {
+        return 'Hello world!';
+    },
+};
+
+var app = express();
+app.use('/internal', graphqlHTTP({
+    schema: schema,
+    rootValue: root,
+    graphiql: true,
+}));
+
+app.listen(port, () => {
+    console.log(`Running a GraphQL API server at /internal on port ${port}`);
+});
