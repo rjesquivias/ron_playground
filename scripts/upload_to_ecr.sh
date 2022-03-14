300a50e081ece7f0b2738302db4c41ce656512d3
RJ Esquivias <esquiviasrj@gmail.com>
2022-03-13 21:11:32 -0700

    Add buildspec for codebuild

--- scripts/upload_to_ecr.sh
+++ scripts/upload_to_ecr.sh
@@ -1,3 +1,5 @@
+#!/bin/bash
+docker build . -t rjcapuchin/ron_playground
+docker tag rjcapuchin/ron_playground:latest 510722588970.dkr.ecr.us-west-2.amazonaws.com/images:latest
 aws ecr get-login-password --region us-west-2 | docker login --username AWS --password-stdin 510722588970.dkr.ecr.us-west-2.amazonaws.com
-docker tag rjcapuchin/ron_playground 510722588970.dkr.ecr.us-west-2.amazonaws.com/images
-docker push 510722588970.dkr.ecr.us-west-2.amazonaws.com/images
+docker push 510722588970.dkr.ecr.us-west-2.amazonaws.com/images:latest
